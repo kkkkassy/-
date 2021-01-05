24478dc129a3f7d1fcbf7ba881f92c69e95a3042
kkkkassy <50669653+kkkkassy@users.noreply.github.com>
2021-01-05 09:26:01 +0800

    Update calculator.js

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,7 +1,13 @@
 window.onload=function(){
 var num = document.getElementsByClassName("num");
-var show=document.getElementById("view");
+var view=document.getElementById("view");
 var equal=document.getElementById("equal");
+var add=document.getElementById("add");
+var subtract=document.getElementById("subtract");
+var multiply=document.getElementById("multiply");
+var divide=document.getElementById("div");
+var res=document.getElementById("reset");
+var percent=document.getElementById("percent");
 var num1;
 var num2;
 var press = 0; 
@@ -10,47 +16,47 @@ var type = 0;
 for (i = 0; i < num.length; i++) {
     num[i].onclick = function() {
         if (press == 0) {
-            show.innerHTML = show.innerHTML + this.value;
+            view.innerHTML = view.innerHTML + this.value;
         } else {
-            show.innerHTML = this.value;
+            view.innerHTML = this.value;
             press = 0;
         }
 
     }
 }
 
-document.getElementById("add").onclick = function() {
+add.onclick = function() {
     type = 0;
     press = 1;
-    num1 = show.innerHTML;
+    num1 = view.innerHTML;
 }
-document.getElementById("subtract").onclick = function() {
+subtract.onclick = function() {
     type = 1;
     press = 1;
-    num1 = show.innerHTML;
+    num1 = view.innerHTML;
 }
-document.getElementById("multiply").onclick = function() {
+multiply.onclick = function() {
     type = 2;
     press = 1;
-    num1 = show.innerHTML;
+    num1 = view.innerHTML;
 }
-document.getElementById("div").onclick = function() {
+divide.onclick = function() {
     type = 3;
     press = 1;
-    num1 = show.innerHTML;
+    num1 = view.innerHTML;
 }
-document.getElementById("reset").onclick = function() {
+res.onclick = function() {
     press = 0;
-    show.innerHTML = null;
+    view.innerHTML = null;
 }
-document.getElementById("percent").onclick = function() {
-    num1 = parseFloat(show.innerHTML);
-    show.innerHTML = num1 * 100 + '%';
+percent.onclick = function() {
+    num1 = parseFloat(view.innerHTML);
+    view.innerHTML = num1 * 100 + '%';
 }
 
 equal.onclick = function() {
-    num2 = show.innerHTML;
-    show.innerHTML = calculate();
+    num2 = view.innerHTML;
+    view.innerHTML = calculate();
 }
 
 function calculate() {
